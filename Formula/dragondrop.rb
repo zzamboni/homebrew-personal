f2d04917ecb0ca8946a2779c2eb58eed37ddbede
Diego Zamboni <diego@zzamboni.org>
2017-03-20 13:23:52 +0100

    Updated Formula to adjust for personal copy of distribution file

--- Formula/dragondrop.rb
+++ Formula/dragondrop.rb
@@ -1,12 +1,10 @@
 cask 'dragondrop' do
   version '1.4.1'
-  sha256 '40e91b83d9a70a6f416394cddb58e758b44fa1c84950f99a6a391f60bce32e81'
+  sha256 '79845d9c9215895267b5aca8f37f4b8c333b15f1df72c82b6fdff652a17395bc'
 
-  url "https://shinyplasticbag.com/dragondrop/releases/dragondrop_#{version}.dmg"
+  url "https://github.com/zzamboni/homebrew-personal/blob/master/files/dragondrop_#{version}_zzamboni.dmg"
   name 'DragonDrop'
-  appcast 'https://shinyplasticbag.com/dragondrop/updates.xml',
-          :sha256 => '5d68912968cd59c8e98393dd5d4781f951d90837efbba42c1ba6fe17135860f0'
-  homepage 'https://shinyplasticbag.com/dragondrop/'
+  homepage 'https://github.com/zzamboni/homebrew-personal'
   license :commercial
 
   app 'DragonDrop.app'
