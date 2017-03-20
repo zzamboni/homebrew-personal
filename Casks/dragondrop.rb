95763c38b355ce841a1b899b2dfa1f010b427687
Diego Zamboni <diego@zzamboni.org>
2017-03-20 15:58:23 +0100

    Updated URL

--- Casks/dragondrop.rb
+++ Casks/dragondrop.rb
@@ -2,7 +2,7 @@ cask 'dragondrop' do
   version '1.4.1'
   sha256 '79845d9c9215895267b5aca8f37f4b8c333b15f1df72c82b6fdff652a17395bc'
 
-  url "https://github.com/zzamboni/homebrew-personal/blob/master/files/dragondrop_#{version}_zzamboni.dmg"
+  url "https://github.com/zzamboni/homebrew-personal/raw/master/files/dragondrop_#{version}_zzamboni.dmg"
   name 'DragonDrop'
   homepage 'https://github.com/zzamboni/homebrew-personal'
   license :commercial
